cc5021cef5f03e4f54104fb92dc7bd08cbfe9edd
Anastasiia Evdokimova <aevdok3@uic.edu>
2021-01-18 23:29:31 -0600

    Memory leaks and warnings fixed

--- Gaus/main.c
+++ Gaus/main.c
@@ -3,12 +3,25 @@
 #include <math.h>
 #include <locale.h>
 
-void main()
+void change(double **nc, int i1, int i2, int n){
+int j;
+double *buf;
+
+buf = (double*) malloc((n+1)*sizeof(double));
+for (j=0; j<n+1;j++)
+{
+    buf[j] = nc[i1][j];
+    nc[i1][j] = nc[i2][j];
+    nc[i2][j] = buf[j];
+}
+}
+
+int main()
 {
 double **data, k, help, *helper, **miss, *mishelp, copy;
 int i, j, l, hlp, n, counter;
 setlocale(LC_ALL, "rus");
-printf("Введите порядок системы: ");
+printf("Р’РІРµРґРёС‚Рµ РїРѕСЂСЏРґРѕРє СЃРёСЃС‚РµРјС‹: ");
 scanf("%d", &n);
 data = (double**)  malloc(n*sizeof(double*));
 miss = (double**)  malloc(n*sizeof(double*));
@@ -17,11 +30,11 @@ for (i = 0; i < n; i++)
     data[i] = (double*)  malloc((n+1)*sizeof(double));
     miss[i] = (double*)  malloc((n+1)*sizeof(double));
 };
-printf("Вводите элементы расширенной матрицы: \n");
+printf("Р’РІРѕРґРёС‚Рµ СЌР»РµРјРµРЅС‚С‹ СЂР°СЃС€РёСЂРµРЅРЅРѕР№ РјР°С‚СЂРёС†С‹: \n");
 for (i = 0; i<n; i++)
     for (j=0; j<n+1; j++)
 {
-  printf("Введите элемент %d-%d: ", i+1, j+1);
+  printf("Р’РІРµРґРёС‚Рµ СЌР»РµРјРµРЅС‚ %d-%d: ", i+1, j+1);
   scanf("%lf", &data[i][j]);
         miss[i][j] = 1;
         copy = data[i][j];
@@ -35,13 +48,13 @@ while (floor(copy)!=ceil(copy))
         miss[i][j] = 0.001;
      if (miss[i][j]==1)   miss[i][j] = 0;
 }
-printf("\nВаша расширенная матрица: ");
+printf("\nР’Р°С€Р° СЂР°СЃС€РёСЂРµРЅРЅР°СЏ РјР°С‚СЂРёС†Р°: ");
 for (i = 0; i<n; i++){
         printf("\n");
     for (j=0; j<n+1; j++)
         printf("%5.5lf ", data[i][j]);}
 
-        printf("\n\nПогрешности элементов: ");
+        printf("\n\nРџРѕРіСЂРµС€РЅРѕСЃС‚Рё СЌР»РµРјРµРЅС‚РѕРІ: ");
 for (i = 0; i<n; i++){
         printf("\n");
     for (j=0; j<n+1; j++)
@@ -60,7 +73,7 @@ for (i = 0; i<n-1; i++){
                     change(data, i, n-hlp, n);
                     change(miss, i, n-hlp, n);
         }hlp++;} while((help==0)||(hlp > n-i));
-if (hlp > n-i) {printf("\n\nСистема несовместна, решений нет"); exit(0); }
+if (hlp > n-i) {printf("\n\nРЎРёСЃС‚РµРјР° РЅРµСЃРѕРІРјРµСЃС‚РЅР°, СЂРµС€РµРЅРёР№ РЅРµС‚"); exit(0); }
     for (j=i+1; j<n; j++){
         k = -data[j][i]/help;
             for (l=i; l<n+1; l++){
@@ -68,14 +81,14 @@ if (hlp > n-i) {printf("\n\nСистема несовместна, решений нет"); exit(0); }
                 data[j][l] = data[i][l]*k+data[j][l];}
         }
         }
-        printf("\n\nРезультат прямого шага Гаусса:");
+        printf("\n\nР РµР·СѓР»СЊС‚Р°С‚ РїСЂСЏРјРѕРіРѕ С€Р°РіР° Р“Р°СѓСЃСЃР°:");
 for (l = 0; l<n; l++){
         printf("\n");
     for (j=0; j<n+1; j++)
         printf("%5.5lf ", data[l][j]);}
 
 
-        printf("\n\nПогрешности первого шага:");
+        printf("\n\nРџРѕРіСЂРµС€РЅРѕСЃС‚Рё РїРµСЂРІРѕРіРѕ С€Р°РіР°:");
 for (l = 0; l<n; l++){
         printf("\n");
     for (j=0; j<n+1; j++)
@@ -84,10 +97,10 @@ for (l = 0; l<n; l++){
 
     helper = (double*) malloc (n*sizeof(double));
     mishelp = (double*) malloc (n*sizeof(double));
-    //Обратный шаг
+    //РћР±СЂР°С‚РЅС‹Р№ С€Р°Рі
 
     if ((data[n-1][n]*data[n-1][n-1]==0)&&((data[n-1][n]!=0||data[n-1][n-1]!=0)))
-        {printf("\n\nСистема несовместна, решений нет"); exit(0); } else {
+        {printf("\n\nРЎРёСЃС‚РµРјР° РЅРµСЃРѕРІРјРµСЃС‚РЅР°, СЂРµС€РµРЅРёР№ РЅРµС‚"); exit(0); } else {
 for (i=n-1;i>=0;i--)
 {
     for (j=0; j<n; j++)
@@ -97,7 +110,7 @@ for (i=n-1;i>=0;i--)
         }
         if((data[i][n]==0)&&(data[i][i]==0))
         {
-            printf("\n\nСистема несовместна, решений нет");
+            printf("\n\nРЎРёСЃС‚РµРјР° РЅРµСЃРѕРІРјРµСЃС‚РЅР°, СЂРµС€РµРЅРёР№ РЅРµС‚");
             exit(0);
         }
     helper[i] = data[i][n]/data[i][i];
@@ -108,21 +121,26 @@ for (i=n-1;i>=0;i--)
         }
 }
 
-printf("\nРешение уравнения: (");
+printf("\nР РµС€РµРЅРёРµ СѓСЂР°РІРЅРµРЅРёСЏ: (");
 for (i=0;i<n;i++)
-    printf("%5.5lf (погрешность %lf) ", helper[i], mishelp[i]);
+    printf("%5.5lf (РїРѕРіСЂРµС€РЅРѕСЃС‚СЊ %lf) ", helper[i], mishelp[i]);
     printf(")");
-}}
+}
 
-void change(double **nc, int i1, int i2, int n){
-int j;
-double *buf;
+//let's free that memory
+free(helper);
+free(mishelp);
 
-buf = (double*) malloc((n+1)*sizeof(double));
-for (j=0; j<n+1;j++)
+for (i = 0; i < n; i++)
 {
-    buf[j] = nc[i1][j];
-    nc[i1][j] = nc[i2][j];
-    nc[i2][j] = buf[j];
+    free(data[i]);
+    free(miss[i]);
+};
+
+free(data);
+free(miss);
+
+return 0;
 }
-}
+
+
